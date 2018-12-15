5451be867f10b8dcdb1632a3be25b37aa174d584
minhquang <barondark998@gmail.com>
2018-12-16 04:26:56 +0700

    update menu

--- src/base/enemy/EnemyExplosion.java
+++ src/base/enemy/EnemyExplosion.java
@@ -13,12 +13,26 @@ public class EnemyExplosion extends GameObject {
     }
     private void createAnimation() {
         ArrayList<BufferedImage> images = SpriteUtils.loadImages(
+<<<<<<< Updated upstream
                 "assets/Image/explosion/0.gif",
                 "assets/Image/explosion/1.gif",
                 "assets/Image/explosion/2.gif",
                 "assets/Image/explosion/3.gif",
                 "assets/Image/explosion/4.gif",
                 "assets/Image/explosion/5.gif"
+=======
+                "assets/Image/explosion/0.png",
+                "assets/Image/explosion/1.png",
+                "assets/Image/explosion/2.png",
+                "assets/Image/explosion/3.png",
+                "assets/Image/explosion/4.png",
+                "assets/Image/explosion/5.png",
+                "assets/Image/explosion/6.png",
+                "assets/Image/explosion/7.png",
+                "assets/Image/explosion/8.png",
+                "assets/Image/explosion/9.png",
+                "assets/Image/explosion/10.png"
+>>>>>>> Stashed changes
         );
         this.renderer = new AnimationRenderer(images
                 , 5, true);
